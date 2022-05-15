14e9fabfd2d19a0b1f5e3d3cba4eb2fbe693a3c0
Chnanda <nc842@drexel.edu>
2022-05-15 08:55:49 -0400

    one more commit

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Drexel-SoftwareArch-SE577
-Software Architecture Graduate Class (SE577)
-
-I am a full stack developer and recently started pursuing into big data analysis. Been with Comcast for more than 4 years and working on different technologies and streams.
-
-I believe anything that is well architected and planned will have fruitful results, coming to software architecture, I would like to know the best architecture methods, models for different use cases which have been proved, don’t want to reinvent the wheel. And with the current trend I would like to explore and how we can utilize and learn the software architecture methods, and implement the same at my work place.
-
+Test Branch
---
+++ src/pages/AppPages/AboutModular.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="q-pa-md">
+    <AboutSection />
+    <LoginSection />
+    <InfoSection />
+    <ReposListSection />
+    <RepoDetailsSection />
+    <EndNotesSection />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AboutSection from 'components/About/AboutSection.vue';
+import LoginSection from 'src/components/About/LoginSection.vue';
+import InfoSection from 'src/components/About/InfoSection.vue';
+import ReposListSection from 'src/components/About/ReposListSection.vue';
+import RepoDetailsSection from 'src/components/About/RepoDetailsSection.vue';
+import EndNotesSection from 'components/About/EndNotesSection.vue';
+
+export default defineComponent({
+  name: 'AboutModularPage',
+  components: {
+    AboutSection,
+    LoginSection,
+    InfoSection,
+    ReposListSection,
+    RepoDetailsSection,
+    EndNotesSection,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
